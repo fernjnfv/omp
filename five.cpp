3f48048a8e2090def8afe2f929cbf80d2eb8f945
fernjnfv <60868815+fernjnfv@users.noreply.github.com>
2020-12-26 15:04:12 +0300

    Отчет + микроправка

--- five.cpp
+++ five.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-const int N = 15000000;
+const int N = 150000000;
 
 void five() {
     //srand(time(NULL));
@@ -25,6 +25,28 @@ void five() {
         C[i] = 1+(mersenne() % 100000);
     }
 
+    start = omp_get_wtime();
+    #pragma omp parallel private(timed2) num_threads(1)
+    {
+        #pragma omp for reduction(+:composition2)
+        for (int i = 0; i < N; i++)
+        {
+            if ((A[i] % 2) == 0)
+            {
+                timed2 = B[i] / C[i];
+            }
+            else
+            {
+                timed2 = B[i] + A[i];
+            }
+            composition2 += timed2;
+        }
+    }
+    finish = omp_get_wtime();
+    cout << finish - start << "s without paralel ";
+    cout << composition2 << endl;
+
+    composition2 = 0;
     start = omp_get_wtime();
     #pragma omp parallel private(timed2) num_threads(2)
     {
@@ -47,10 +69,13 @@ void five() {
     cout << composition2 << endl;
 
 
+    omp_set_num_threads(12);
+    double* sum = new double[2];
+    sum[0]=0;
+    sum[1]=0;
     composition2 = 0;
     start = omp_get_wtime();
-    //omp_set_num_threads(12);
-    #pragma omp  parallel sections private(timed2)
+    #pragma omp  parallel sections private(timed2) num_threads(12)
     {
         #pragma omp section
         {
@@ -64,10 +89,7 @@ void five() {
                 {
                     timed2 = B[i] + A[i];
                 }
-                #pragma omp critical(check)
-                {
-                    composition2 += timed2;
-                }
+                sum[0] += timed2;
             }
         }
         #pragma omp section
@@ -82,24 +104,28 @@ void five() {
                 {
                     timed2 = B[i] + A[i];
                 }
-                #pragma omp critical(check)
-                {
-                    composition2 += timed2;
-                }
+                sum[1] += timed2;
             }
         }
     }
+    composition2 = sum[0] + sum[1];
     finish = omp_get_wtime();
     cout << finish - start << "s with 2 section ";
     cout << composition2 << endl;
 
+
+    sum = new double[4];
     composition2 = 0;
+    sum[0]=0;
+    sum[1]=0;
+    sum[2]=0;
+    sum[3]=0;
     start = omp_get_wtime();
-    #pragma omp parallel sections private(timed2)
+    #pragma omp parallel sections private(timed2) num_threads(12)
     {
         #pragma omp section
         {
-            for (int i = 0; i < N/2; i++)
+            for (int i = 0; i < N/4; i++)
             {
                 if ((A[i] % 2) == 0)
                 {
@@ -109,15 +135,12 @@ void five() {
                 {
                     timed2 = B[i] + A[i];
                 }
-                #pragma omp critical(check)
-                {
-                    composition2 += timed2;
-                }
+                sum[0] += timed2;
             }
         }
         #pragma omp section
         {
-            for (int i = N/2; i < N/4*2; i++)
+            for (int i = N/4; i < N/4*2; i++)
             {
                 if ((A[i] % 2) == 0)
                 {
@@ -127,10 +150,7 @@ void five() {
                 {
                     timed2 = B[i] + A[i];
                 }
-                #pragma omp critical(check)
-                {
-                    composition2 += timed2;
-                }
+                sum[1] += timed2;
             }
         }
         #pragma omp section
@@ -145,10 +165,7 @@ void five() {
                 {
                     timed2 = B[i] + A[i];
                 }
-                #pragma omp critical(check)
-                {
-                    composition2 += timed2;
-                }
+                    sum[2] += timed2;
             }
         }
         #pragma omp section
@@ -163,13 +180,11 @@ void five() {
                 {
                     timed2 = B[i] + A[i];
                 }
-                #pragma omp critical(check)
-                {
-                    composition2 += timed2;
-                }
+                    sum[3] += timed2;
             }
         }
     }
+    composition2 = sum[0] + sum[1]+sum[2]+sum[3];
     finish = omp_get_wtime();
     cout << finish - start << "s with 4 section ";
     cout << composition2 << endl;
